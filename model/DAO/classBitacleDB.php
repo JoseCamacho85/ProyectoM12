3deca0916c707ff80963bd5d6969f214e9bcc034
amazuelos <amazuelos92@gmail.com>
2016-05-02 16:32:53 +0200

    clases en fase beta

---
+++ model/DAO/classBitacleDB.php
@@ -0,0 +1,16 @@
+<?php
+include("../../dbConnector.php");
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of classBitacleDB
+ *
+ * @author Mazu
+ */
+class classBitacleDB{
+    
+}
---
+++ model/DAO/classDB.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of classDB
+ *
+ * @author Mazu
+ */
+class classDB extends PDO{
+    private $host;
+    private $user;
+    private $pass;
+    private $dbname;
+ 
+    private $dbh;
+    private $error;
+ 
+    public function __construct(){
+        
+        $this->setHost($GLOBALS['server']);
+        $this->setUser($GLOBALS['USER']);
+        $this->setPass($GLOBALS['PASS']);
+        $this->setDbName($GLOBALS['dbname']);
+        $this->Open();
+    }
+
+    public function Open(){
+        try{
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        parent::__construct($dsn,$this->getUser(),$this->getPass());       
+        }catch(Exception $e){
+            echo "ERROR AL CREAR CONEXION";
+        }
+    }
+    
+    public function executeNonQuery($nonquery){
+        $nonquery->execute();
+    }
+    public function executeQuery($query){
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $results;
+    }
+
+    public function getDbName(){
+        return $this->dbname;
+    }
+
+    public function setDbName($value){
+        return $this->dbname=$value;
+    }
+
+    public function getHost(){
+        return $this->host;
+    }
+    
+    public function setHost($value){
+        $this->host = $value;
+    }
+    
+    public function getUser(){
+        return $this->user;
+    }
+
+    public function setUser($value){
+        $this->user = $value;
+    }
+    
+    public function getPass(){
+        return $this->pass;
+    }
+
+    public function setPass($value){
+        $this->pass = $value;
+    }
+    
+}
