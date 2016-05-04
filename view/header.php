2f820aea31daea3e43856cdc7660330b08b36acc
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-04 19:46:14 +0200

    glyphicons updated

--- view/header.php
+++ view/header.php
@@ -23,14 +23,14 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>                        
                 </button>
-                <a class="navbar-brand" href="main.php">Logo</a>
+                <a class="navbar-brand" href="main.php">BitaCle</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="main.php"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
-                    <li><a href="showPOI.php">POI's</a></li>
-                    <li><a href="showRutas.php">Rutas</a></li>
-                    <li><a href="showForo.php">Foro</a></li>
+                    <li><a href="showPOI.php"><span class="glyphicon glyphicon-cutlery"></span> POI's</a></li>
+                    <li><a href="showRutas.php"><span class="glyphicon glyphicon-road"></span> Rutas</a></li>
+                    <li><a href="showForo.php"><span class="glyphicon glyphicon-comment"></span> Foro</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a>Usuario: <input type="text" name="user" class="userInput" required></a></li>
