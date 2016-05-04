793d376be8b21f3ac2f4ebbdf4917769e0255ec1
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-04 19:26:06 +0200

    añadido main, header y footer

--- view/header.php
+++ view/header.php
@@ -2,18 +2,42 @@
 <html>
     <head>
         <title>Bitacle -- Tu red de viajes</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <link type="text/css" href="css/estilos.css">
         <script src="js/jquery.js"></script>
         <script src="js/animation.js"></script>
         <script src="js/validations.js"></script>
+
     </head>
-    
-    <navbar>
-        <form id="userForm" method="POST" action="../controller/controllerUser.php">
-            <img id="logo" src=""/>
-            <input id="user" type="TEXT" class="userInput" required>
-            <input id="password" type="PASSWORD" class="userInput" required>
-            <input type="submit" id="ok" value="logeate"/><br>
-            <label id="userError"></label><label id="userError"></label><br>
-            <a href="register.php">Registrate</a>
-        </form>
-    </navbar>
+<body>
+    <!--INICIO DE NAVBAR-->
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="main.php">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="main.php" class="glyphicon glyphicon-home">Inicio</a></li>
+                    <li><a href="showPOI.php">POI's</a></li>
+                    <li><a href="showRutas.php">Rutas</a></li>
+                    <li><a href="showForo.php">Foro</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a>Usuario: <input type="text" name="user" class="userInput" required></a></li>
+                    <li><a>Password: <input type="password" name="pass" class="userInput" required></a></li>
+                    <li><a href="../controller/controllerUser.php" id="ok"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                     <li><a href="register.php" id="ok"><span class="-glyphicon glyphicon-plus"></span> Signin</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
