ef4315da62121ac3146c250167ebb1e840c91439
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-09 17:04:24 +0200

    modificaciones fonts, añadidos algunos js

--- view/header.php
+++ view/header.php
@@ -6,6 +6,12 @@ include ("../controller/checkSession.php");
     <head>
         <title>Bitacle -- Tu red de viajes</title>
         <meta charset="utf-8">
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
--- view/main.php
+++ view/main.php
@@ -8,9 +8,27 @@ include ("header.php");
             <p>DESCRIPCION DEL WEB</p>
             <hr>
             <h3>TITULO</h3>
-            <p>CONTENIDO AQUÍ</p>
-</div>
+            <div class="flexslider">
+                <ul class="slides">
+                    <li>
+                        <img src="img/zelda2.jpg" />
+                    </li>
+                    <li>
+                        <img src="img/zelda3.jpg" />
+                    </li>
+                    <li>
+                        <img src="img/zelda4.jpg" />
+                    </li>
+                    <li>
+                        <img src="img/zelda5.jpg" />
+                    </li>
+                    <li>
+                        <img src="img/zelda6.jpg" />
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-<?php
-include ("footer.php");
-?>
+        <?php
+        include ("footer.php");
+        ?>
