a2aa47188ae2f26dbddf1f71c1e4a98dc4e6bdf8
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-11 15:23:00 +0200

    showUser

--- view/showUser.php
+++ view/showUser.php
@@ -1,6 +1,12 @@
 <?php
 include ("header.php");
 ?>
+<div>
+    <p><input type="button" value="Datos" onclick="location.href = 'showDataUser.php'"></p>
+    <p><input type="button" value="Diario" onclick="location.href = 'showDiaryUser.php'"></p>
+    <p><input type="button" value="POI's y Ciudades visitadas" onclick="location.href = 'showPOICityUser.php'"></p>
+
+</div>
 <?php
 include ("footer.php");
 ?>
