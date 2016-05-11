049ff91c4aa184d3a000f96243f531337af82ae0
rokatto <joanroca81@gmail.com>
2016-05-11 17:37:01 +0200

    Login OK
    Falta el error al fallar

--- view/modules/userNav.php
+++ view/modules/userNav.php
@@ -1,4 +1,4 @@
-<?php $user = "prueba user"; //$bitacle->getUserLoged(); ?>
 <ul class="nav navbar-nav navbar-right">
     <li><a href="mainUser.php"><?php echo $user;?></a></li>
+    <li><a href="../controller/logout.php"><span class="glyphicon glyphicon-log-out"> Salir</span></li>
 </ul>
