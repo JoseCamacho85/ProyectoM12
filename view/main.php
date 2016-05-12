e37b60e1616217438e4b37f52adee54717efb582
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-12 17:13:34 +0200

    arreglado titulo con slider

--- view/main.php
+++ view/main.php
@@ -2,14 +2,12 @@
 include ("header.php");
 ?>
 <div class="container-fluid text-center">
-
     <div class="row ">
-        <div class="col-sm-12 text-center">
-            <div id="tituloPagina">
+        <div id="tituloPagina">
                 <h1>BitaCle</h1>
                 <p>DESCRIPCION DEL WEB</p>
-                <hr>
             </div>
+        <div class="col-md-12">
             <div class="flexslider">
                 <ul  id="sliderContent" class="slides">
                     <li>
