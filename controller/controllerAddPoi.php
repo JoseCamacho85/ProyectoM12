28d4c07392b25fb00452bb54fd0f664348268db4
amazuelos <amazuelos92@gmail.com>
2016-05-13 17:05:54 +0200

    validar que no se entre en controllerAddPOI sin insertar

--- controller/controllerAddPoi.php
+++ controller/controllerAddPoi.php
@@ -3,6 +3,7 @@
 session_start();
 include("../model/functionAutoLoad.php");
 include("controllerIdDropdowns.php");
+include ('validateNullFields.php');
 $bitacle = unserialize($_SESSION['bitacle']);
 
 if (isset($_REQUEST['submit'])) {
@@ -18,26 +19,39 @@ if (isset($_REQUEST['submit'])) {
     $id_ciudad = $_REQUEST['ciudadPoi'];
     $id_pais = $_REQUEST['paisPoi'];
     $id_usuario = 1 /* $_REQUEST['usuarioPoi'] */;
+    
+    $requiredFields[] = $nombre;
+    
+    
+    
+        if(validateNullfields($requiredFields)){
+            $tipo = $bitacle->getTipos();
+            $transporte = $bitacle->getTransportes();
+            $entorno = $bitacle->getEntornos();
+            $ciudad = $bitacle->getCiudades();
+            $pais = $bitacle->getPaises();
+            //    $usuario = $bitacle->getUsers();
 
-    $tipo = $bitacle->getTipos();
-    $transporte = $bitacle->getTransportes();
-    $entorno = $bitacle->getEntornos();
-    $ciudad = $bitacle->getCiudades();
-    $pais = $bitacle->getPaises();
-//    $usuario = $bitacle->getUsers();
+            $id_tipo1 = cogerId($tipo, $id_tipo);
+            $id_transporte1 = cogerId($transporte, $id_transporte);
+            $id_entorno1 = cogerId($entorno, $id_entorno);
+            $id_ciudad1 = cogerId($ciudad, $id_ciudad);
+            $id_pais1 = cogerId($pais, $id_pais);
+            //    $id_usuario1 = cogerId($usuario, $id_usuario);
 
-    $id_tipo1 = cogerId($tipo, $id_tipo);
-    $id_transporte1 = cogerId($transporte, $id_transporte);
-    $id_entorno1 = cogerId($entorno, $id_entorno);
-    $id_ciudad1 = cogerId($ciudad, $id_ciudad);
-    $id_pais1 = cogerId($pais, $id_pais);
-//    $id_usuario1 = cogerId($usuario, $id_usuario);
-
-    try {
-        $bitacle->insertPoi(null, $nombre, $foto, $descripcion, $url, $precio, $horario, $id_tipo1, $id_transporte1, $id_entorno1, $id_ciudad1, $id_pais1, $id_usuario);
-        echo $nombre . "perfecto";
-    } catch (Exception $e) {
-        showMessage($e->getMessage());
+            try {
+                $bitacle->insertPoi(null, $nombre, $foto, 
+                        $descripcion, $url, $precio, $horario, 
+                        $id_tipo1, $id_transporte1, $id_entorno1, 
+                        $id_ciudad1, $id_pais1, $id_usuario);
+                echo $nombre . "perfecto";
+            } catch (Exception $e) {
+                showMessage($e->getMessage());
+        }
+    }else{
+        echo "Requiere que al menos introducir el nombre";
     }
+}else{
+    header("location:../view/addPOI.php");
 }
 ?>
