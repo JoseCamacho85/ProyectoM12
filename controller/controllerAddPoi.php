85157ed43de5ecb3a95889a74cb6a319ee9f56f0
rokatto <joanroca81@gmail.com>
2016-05-14 16:10:39 +0200

    id de usuario automatico
    Ya aplica el id del usuario logeado cuando creas un poi

--- controller/controllerAddPoi.php
+++ controller/controllerAddPoi.php
@@ -7,7 +7,7 @@ include("validateNullfields.php");
 include("validateNames.php");
 include("validateDescriptions.php");
 $bitacle = unserialize($_SESSION['bitacle']);
-
+$user = unserialize($_SESSION['user']);
 
 
 $nombre = $_POST['nombrePoi'];
@@ -21,22 +21,22 @@ $id_transporte = $_POST['transportePoi'];
 $id_entorno = $_POST['entornoPoi'];
 $id_ciudad = $_POST['ciudadPoi'];
 $id_pais = $_POST['paisPoi'];
-$id_usuario = 5 /* $_POST['usuarioPoi'] */;
 
+//aplicar id de usuario logeado
+$usuarios = $bitacle->getUsers();
+$id_usuario = cogerId($usuarios,$user);
 
 $tipo = $bitacle->getTipos();
 $transporte = $bitacle->getTransportes();
 $entorno = $bitacle->getEntornos();
 $ciudad = $bitacle->getCiudades();
 $pais = $bitacle->getPaises();
-//    $usuario = $bitacle->getUsers();
 
 $id_tipo1 = cogerId($tipo, $id_tipo);
 $id_transporte1 = cogerId($transporte, $id_transporte);
 $id_entorno1 = cogerId($entorno, $id_entorno);
 $id_ciudad1 = cogerId($ciudad, $id_ciudad);
 $id_pais1 = cogerId($pais, $id_pais);
-//    $id_usuario1 = cogerId($usuario, $id_usuario);
 
 $requiredFields = Array($nombre, $descripcion);
 
@@ -44,14 +44,14 @@ $requiredFields = Array($nombre, $descripcion);
 if (validateNullfields($requiredFields) 
         && validateNames($nombre) 
         && validateDescriptions($descripcion)
-        && validateUrls($url)) {
+        /*&& validateUrls($url)*/) {
     try {
         $bitacle->insertPoi(null, $nombre, $foto, $descripcion, $url, $precio, $horario, $id_tipo1, $id_transporte1, $id_entorno1, $id_ciudad1, $id_pais1, $id_usuario);
         echo $nombre . " INSERTADO CORRECTAMENTE";
     } catch (Exception $e) {
         showMessage($e->getMessage());
     }
-} else {
-    echo "error";
+//} else {
+    //echo "error";
 }
 ?>
