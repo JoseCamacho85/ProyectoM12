672de68818e849f904fcff2fe552afa2e2bd7969
reiixa <raquelibanezs@gmail.com>
2016-05-17 18:59:45 +0200

    diario

--- controller/controllerVerDetalleUsuario.php
+++ controller/controllerVerDetalleUsuario.php
@@ -9,9 +9,16 @@ try {
     $id_usuario = cogerIdUsuario($usuarios, $user);
 
     $usuario = $bitacle->VerDetallesUsuario($id_usuario);
-    
+
     $username = $usuario->getUsername();
+    $email = $usuario->getEmail();
+    $poblacion = $usuario->getPoblacion();
+    $idioma = $usuario->getIdioma();
+    $telefono = $usuario->getTelefono();
+    $url = $usuario->getUrl();
+    $texto = $usuario->getTextoPresentacion();
+    $foto = $usuario->getFoto();
 } catch (Exception $ex) {
-    
+    $ex->getMessage();
 }
 ?>
--- view/mainUser.php
+++ view/mainUser.php
@@ -3,11 +3,17 @@ include ("header.php");
 include '../controller/controllerVerDetalleUsuario.php';
 ?>
 <h3>DATOS PERSONALES</h3>
-<p>Username: <?php echo $username ?> </p>
-<p>E-mail: <?php ?> </p>
-<p>Población: <?php ?> </p>
-<p>Idioma: <?php ?> </p>
-<p>Teléfono: <?php ?> </p>
-<p>URL: <?php ?> </p>
-<p>Texto de presentación: <?php ?> </p>
+<form method="post" action="../controller/controllerModificarDatosUsuario.php"> <!-- HAY QUE CREAR EL CONTROLLER -->
+    <img src="<?php $foto ?>" />
+    <p>Username: <input type="text" value="<?php echo $username ?>" /></p>
+    <p>E-mail: <input type="text" value="<?php echo $email ?>" </p>
+    <p>Población: <input type="text" value="<?php echo $poblacion ?>" </p>
+    <p>Idioma: <input type="text" value="<?php echo $idioma ?>" </p>
+    <p>Teléfono: <input type="text" value="<?php echo $telefono ?>" </p>
+    <p>URL: <input type="text" value="<?php echo $url ?>" </p>
+    <p>Texto de presentación: <input type="text" value="<?php echo $texto ?>" </p>
+    <p><input type="submit" value="Modificar datos" /></p>
+</form>
+<a href="showDiaryUser.php"><button class="btn btn-info">Diarios</button></a>
+<a href="showPOICityUser.php"><button class="btn btn-info">Lugares visitados</button></a>
 <?php include ("footer.php"); ?>
