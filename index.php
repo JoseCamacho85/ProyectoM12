f36606e2e050154fc7ef9a30d6d8c2ec95313c44
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-18 21:10:31 +0100

    redireccionamientos OKEY MAKEY

--- index.php
+++ index.php
@@ -21,5 +21,5 @@ if (isset($_SESSION['bitacle'])) {
     // if session exist, we unserializate it.
     $bitacle = unserialize($_SESSION['bitacle']);
 }
-header("Location:view/main.php");
+header("Location:view/mainUser.php");
 ?>
