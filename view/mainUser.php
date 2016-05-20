f7403684a6be10ed0f48374a4738680d57bcdf0f
reiixa <raquelibanezs@gmail.com>
2016-05-20 17:28:15 +0200

    update modificarDatosUsuario chachi (excepto foto) :ghost:

--- view/mainUser.php
+++ view/mainUser.php
@@ -4,14 +4,14 @@ include '../controller/controllerVerDetalleUsuario.php';
 ?>
 <h2>DATOS PERSONALES</h2>
 <form method="post" action="../controller/controllerModificarDatosUsuario.php">
-    <img id="foto" class="fotoMostrar" src="fotoUsuario/<?php $foto ?>"/>
-    <p>Username: <input id="username" type="text" value="<?php echo $username ?>" /></p>
-    <p>E-mail: <input id="email" type="text" value="<?php echo $email ?>" </p>
-    <p>Población: <input id="poblacion" type="text" value="<?php echo $poblacion ?>" </p>
-    <p>Idioma: <input id="idioma" type="text" value="<?php echo $idioma ?>" </p>
-    <p>Teléfono: <input id="telefono" type="text" value="<?php echo $telefono ?>" </p>
-    <p>URL: <input id="url" type="text" value="<?php echo $url ?>" </p>
-    <p>Texto de presentación: <input id="textoPresentacion" type="text" value="<?php echo $texto ?>" </p>
+    <img class="fotoMostrar" src="fotoUsuario/<?php $foto ?>"/>
+    <p>Username: <input name="username" type="text" value="<?php echo $username ?>" /></p>
+    <p>E-mail: <input name="email" type="text" value="<?php echo $email ?>" </p>
+    <p>Población: <input name="poblacion" type="text" value="<?php echo $poblacion ?>" </p>
+    <p>Idioma: <input name="idioma" type="text" value="<?php echo $idioma ?>" </p>
+    <p>Teléfono: <input name="telefono" type="text" value="<?php echo $telefono ?>" </p>
+    <p>URL: <input name="url" type="text" value="<?php echo $url ?>" </p>
+    <p>Texto de presentación: <input name="textoPresentacion" type="text" value="<?php echo $texto ?>" </p>
     <p><input type="submit" value="Modificar datos" /></p>
 </form>
 <a href="showDiaryUser.php"><button class="btn btn-info">Diarios</button></a>
