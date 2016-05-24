e61d2c32f424da99867ae9f533da628b3ce68745
jordivaleri <jordivalerijane@gmail.com>
2016-05-23 18:34:05 -0700

    comentaris

--- view/mostrarCiudadesVisitadas.php
+++ view/mostrarCiudadesVisitadas.php
@@ -17,6 +17,9 @@ try {
     $usuarios = $bitacle->getUsers();
     $id = cogerIdUsuario($usuarios, $user);
 
+//Query que s'ha de fer en objectes
+   //select ciudad.nombre from ciudad,poi,historial,diario where historial.estaEnPoi = 1 and ciudad.id = poi.id_ciudad and diario.id_usuario = 5 
+
 //arrayPois array de els pois del usuari
     for ($i = 0; $i <count($bitacle->getDiarios());$i++){
     	if ($bitacle->getDiarios()[$i]->getId_usuario() == $id){
@@ -24,17 +27,13 @@ try {
     	}
     }
    
-//Query que s'ha de fer en objectes
-   //select ciudad.nombre from ciudad,poi,historial,diario where historial.estaEnPoi = 1 and ciudad.id = poi.id_ciudad and diario.id_usuario = 5 
-   
-
   //arrayHistorial historial dels pois marcats en visitats i del diari del usuari marcat 
   for ($i = 0; $i <count($bitacle->getHistorial());$i++){
     	if ($bitacle->getHistorial()[$i]->getEstaEnPoi() == "0"){
     		array_push($arrayHistorial,$bitacle->getHistorial()[$i]);
     	}
     }
-    
+    //arrayResultado que compara els diaris i els historials
  for ($i = 0; $i <count($arrayHistorial);$i++){
  	for ($j = 0; $j <count($arrayDiarios);$j++){
     	if ($arrayHistorial[$i]->getId_diario()== $arrayDiarios[$j]->getId()){
@@ -43,7 +42,7 @@ try {
     }
 }
    
-
+//arraysPois que tiene todos los pois del historial del usuario
   for ($i = 0; $i <count($bitacle->getPois());$i++){
  	for ($j = 0; $j <count($arrayResultado);$j++){
     	if ($arrayResultado[$j]->getId_poi()== $bitacle->getPois()[$i]->getId()){
@@ -52,7 +51,7 @@ try {
     }
 }
 
-
+//arrayCiudades amb el nom de les ciutats de la array de pois
 for ($i = 0; $i <count($arrayPois);$i++){
 		$nomCiutat = cogerNombre($bitacle->getCiudades(), $arrayPois[$i]->getId_ciudad());
     	array_push($arrayCiudades,$nomCiutat);   	
