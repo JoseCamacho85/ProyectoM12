06064bef093d70de089e32f81181dd5003628538
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-24 13:21:01 +0100

    añadir fotos a anuncio

--- view/addAnuncio.php
+++ view/addAnuncio.php
@@ -4,7 +4,6 @@ include ("makeDropdownLists.php");
 include("../controller/controllerIdDropdowns.php");
 
 //include ("../controller/controllerCamposAnuncio.php");
-
 ?>
 <style type="text/css">
     #crearAnuncio{
@@ -14,39 +13,46 @@ include("../controller/controllerIdDropdowns.php");
         display:none;
     }
 </style>
-<script type="text/javascript"><?php include ("js/mostrarInfoAnuncio.js");?></script>
+<script type="text/javascript"><?php include ("js/mostrarInfoAnuncio.js"); ?></script>
 <div class="container-fluid text-center">    
     <div class="row content">
         <div class="col-sm-12 text-center"> 
             <h2>Introducir Anuncio</h2>
             <div>
+                <?php
+                    include ("./modules/addFotoAnuncio.php");
+                    ?>
+                <p><span>Foto: </span><input type="text" name="fotoPoi" id="fotoPoi" value="<?php echo $fotoSubida; ?>"></p>
+                <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" enctype="multipart/form-data" name="inscripcion">
+                    <input class="center-block" type="file" name="archivo[]" multiple="multiple">
+                    <input type="submit" value="Subir imagen"  class="trig">
+                </form>
                 <form action="../controller/controllerAddAnuncio.php" name="formPOI">
                     <hr>
                     <p>POI:<select id="POIAnuncio" name="POIAnuncio" onchange="mostrarInfoAnuncio(this.value)">
                             <!--<option>
                                 selecciona POI
                             </option>-->
-                            <?php     
-                            
-                            makeDropdownlistPoisUser();      ?>
+                            <?php makeDropdownlistPoisUser(); ?>
                         </select></p>
                     <p>Titulo:<input type="text" id="tituloAnuncio" name="tituloAnuncio" ></p>
                     <p>Descripción:<input type="text" id="descripcionAnuncio" name="descripcionAnuncio"></p>
-                    <p>Imágen:<input type="text" id="imagenAnuncio" name="imagenAnuncio"></p>
-                    
+                    <p><span>Foto: </span><input type="text" name="imagenAnuncio" id="imagenAnuncio" value="<?php echo $fotoSubida; ?>"></p>
+
                     <input type="submit" id="modificarAnuncio" name= "modificarAnuncio" value="Modificar Anuncio">
                     <div id="botones">
-                    <input type="submit" id="crearAnuncio" name="crearAnuncio" value="Crear Anuncio">
-                </div>
+                        <input type="submit" id="crearAnuncio" name="crearAnuncio" value="Crear Anuncio">
+                    </div>
                 </form>
-                
+
+
 
             </div>
             <div id="seleccionados">
                 ELEMENTOS SELECCIONADOS APARECERÁN AQUÍ
             </div>
             <div id="prueba1">
-                </div>
+            </div>
         </div>
     </div>
 </div>
