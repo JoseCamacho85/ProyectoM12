6f51d9484be69370d1525b5a7bda8518ef6cc141
rokatto <joanroca81@gmail.com>
2016-05-25 16:35:14 +0200

    asd

--- controller/validations/validateNombrePoi.php
+++ controller/validations/validateNombrePoi.php
@@ -1,6 +1,7 @@
 <?php
 function validateNombrePoi($string) {
     $ok = false;
+    if (preg_match('/^[a-zA-Z0-9ÀÁÈÉÌÍÒÓÙÚàáèéìíòóùúñ]{2}+[a-zA-Z0-9_-ÀÁÈÉÌÍÒÓÙÚàáèéìíòóùúñ\s]{0,48}$/', $string)) {
         $ok = true;
     }
     return $ok;
