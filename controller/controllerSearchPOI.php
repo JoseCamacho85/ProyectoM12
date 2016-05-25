bf45e42a09969245c2b0797b4d57c89b6d435e89
jordivaleri <jordivalerijane@gmail.com>
2016-05-25 14:13:59 -0700

    ya se a solucionado el buscador

--- controller/controllerSearchPOI.php
+++ controller/controllerSearchPOI.php
@@ -28,36 +28,36 @@ $arrayCiudad = $bitacle->getCiudades();
 if (isset($_REQUEST['checkTipoPOI'])) {
     //echo $_REQUEST['selectTipoPOI'];
     $tipo = $_REQUEST['selectTipoPOI'];
-    $id_tipo = cogerId($arrayTipo, $tipo);
-    array_push($fields, $id_tipo);
+    //$id_tipo = cogerId($arrayTipo, $tipo);
+    array_push($fields, $tipo);
     array_push($fieldNames, "id_tipo");
 }
 if (isset($_REQUEST['checkTransportePOI'])) {
     //echo $_REQUEST['selectTransportePOI'];
     $transporte = $_REQUEST['selectTransportePOI'];
-    $id_transporte = cogerId($arrayTransporte, $transporte);
-    array_push($fields, $id_transporte);
+    //$id_transporte = cogerId($arrayTransporte, $transporte);
+    array_push($fields, $transporte);
     array_push($fieldNames, "id_transporte");
 }
 if (isset($_REQUEST['checkEntornoPOI'])) {
     //echo $_REQUEST['selectEntornoPOI'];
     $entorno = $_REQUEST['selectEntornoPOI'];
-    $id_entorno = cogerId($arrayEntorno, $entorno);
-    array_push($fields, $id_entorno);
+    //$id_entorno = cogerId($arrayEntorno, $entorno);
+    array_push($fields, $entorno);
     array_push($fieldNames, "id_entorno");
 }
 if (isset($_REQUEST['checkPaisPOI'])) {
     //echo $_REQUEST['selectPaisPOI'];
     $pais = $_REQUEST['selectPaisPOI'];
-    $id_pais = cogerId($arrayPais, $pais);
-    array_push($fields, $id_pais);
+    //$id_pais = cogerId($arrayPais, $pais);
+    array_push($fields, $pais);
     array_push($fieldNames, "id_pais");
 }
 if (isset($_REQUEST['checkCiudadPOI'])) {
     //echo $_REQUEST['selectCiudadPOI'];
     $ciudad = $_REQUEST['selectCiudadPOI'];
-    $id_ciudad = cogerId($arrayCiudad, $ciudad);
-    array_push($fields, $id_ciudad);
+    //$id_ciudad = cogerId($arrayCiudad, $ciudad);
+    array_push($fields, $ciudad);
     array_push($fieldNames, "id_ciudad");
 }
 //echo $_REQUEST['selectTipoPOI'];
