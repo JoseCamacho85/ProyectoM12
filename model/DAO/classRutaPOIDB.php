dee4766c121a1fcd321e783db2f02612ace21da4
jordivaleri <jordivalerijane@gmail.com>
2016-05-26 15:21:14 -0700

    ultimo retoke comentando cosas

--- model/DAO/classRutaPOIDB.php
+++ model/DAO/classRutaPOIDB.php
@@ -1,36 +1,24 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of classRutaPOIDB
- *
- * @author Mazu
- */
 class RutaPOIDB {
 
 
-    
     public function insertRutaPOI($idPOI, $idRuta) {
         $rutaPOI = new RutaPOI($idPOI, $idRuta);
 
         $con = new DB();
         $nonquery = $con->prepare("INSERT INTO rutapoi (id_ruta,id_poi) VALUES (:id_ruta,:id_poi)");
-        $idPOI = $rutaPOI->getIdPOI();
-        $idRuta = $rutaPOI->getIdRuta();
+        //$idPOI = $rutaPOI->getIdPOI();
+        //$idRuta = $rutaPOI->getIdRuta();
 
         $nonquery->bindParam(":id_ruta", $idRuta);
         $nonquery->bindParam(":id_poi", $idPOI);
 
         $con->executeNonQuery($nonquery);
 
-        $id = $con->lastInsertId();
+        //$id = $con->lastInsertId();
 
-        return $id;
+        //return $id;
         $con = null; 
     }
 
