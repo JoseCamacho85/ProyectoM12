f7763e046c46ac0a4c7a8a80f16bcbbd51c6e3b8
JoseCamacho85 <josecamacho85@gmail.com>
2016-05-27 16:58:48 +0100

    ok rutas y diarios

--- controller/controllerAddRutaPOI.php
+++ controller/controllerAddRutaPOI.php
@@ -1,24 +1,30 @@
 <?php
+
 //echo "HOLHA K TAL";
 session_start();
 include("../model/functionAutoLoad.php");
 include("../model/DAO/classRutaPOIDB.php");
 
 $bitacle = unserialize($_SESSION['bitacle']);
-//$user = unserialize($_SESSION['user']);
 
 $idPOI = $_POST["idPoi"];
 $idRuta = $_POST["idRuta"];
+$btnRuta = $_POST["#addRuta"];
 
-//$rutaPOIObj = new RutaPOIDB();
 echo $idPOI;
 echo $idRuta;
-try {
-    $bitacle->insertRutaPoi($idPOI, $idRuta);
-    echo "rutaPoi insertado";
-    header("location:../view/showPOI.php");
-} catch (Exception $e) {
-    echo "error al insertar";
-    showMessage($e->getMessage());
+
+if (isset($btnRuta)) {
+    try {
+        $bitacle->insertRutaPoi($idPOI, $idRuta);
+        echo "rutaPoi insertado";
+        header("location:../view/showPOI.php");
+    } catch (Exception $e) {
+        echo "error al insertar";
+        showMessage($e->getMessage());
+    }
+} else {
+
+    header("location:../view/showDetallePoi.php?id=" . $idPOI . "");
 }
 ?>
