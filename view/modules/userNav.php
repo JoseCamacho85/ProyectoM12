81ae3f7a4f0a8efc5fb0a55b428098afbc593070
Raquel Ibáñez Sánchez <raquelibanezs@gmail.com>
2016-05-29 00:42:50 +0200

    Tipo de letra "salir"

--- view/modules/userNav.php
+++ view/modules/userNav.php
@@ -1,4 +1,4 @@
 <ul class="nav navbar-nav navbar-right">
-    <li><a href="mainUser.php"><span class="glyphicon"><?php echo $user;?></span></a></li>
-    <li><a href="../controller/logout.php"><span class="glyphicon glyphicon-log-out"> Salir</span></a></li>
+    <li><a href="mainUser.php"><span><?php echo $user; ?></span></a></li>
+    <li><a href="../controller/logout.php"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
 </ul>
